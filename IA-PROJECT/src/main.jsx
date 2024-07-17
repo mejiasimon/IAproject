6b88e6d052a6c8085da78c7c0ab49fa3223494a3
mejiasimon <97535209+mejiasimon@users.noreply.github.com>
2024-07-17 15:27:44 -0500

    nuevo

--- IA-PROJECT/src/main.jsx
+++ IA-PROJECT/src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Rutas } from './routes/rutas.jsx'
-
+import '../src/app.css'
+import '../src/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
